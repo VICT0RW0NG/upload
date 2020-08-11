cc10101693b4794c6757aca8fc9bd0ede8267c77
Victor Wong <victorwong49@gmail.com>
2020-08-11 14:23:24 +0800

    Use Tempate to rework the task and also added some addtional infomration which persisted in the log file with AJAX call

--- upload/server.go
+++ upload/server.go
@@ -10,12 +10,23 @@ import (
 	"time"
 	"strconv"
 	"io/ioutil"
+    "html/template"
+	"path"
+	"os"
 )
+
+type UploadResult struct {
+  SubmitTime    string
+  Name 			string
+  FileSize 		string
+}
+
 
 func main(){	
 	// ex.NewTestStorage implements the "osin.Storage" interface
 
 	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/saveResult", saveResult)
 	http.HandleFunc("/readlog", readlog)
 	
 	fmt.Println("init...") 
@@ -24,17 +35,22 @@ func main(){
 	
 func upload (w http.ResponseWriter, r *http.Request) {
 	
-	
     if r.Method == "GET" {
     	
-    	w.Header().Set("diffResult", r.Header.Get("diffResult"))
-
-        http.ServeFile(w, r, "upload/upload.html")
-    } else {
-    //	start := time.Now()
-
+    	fp := path.Join("templates", "upload.html")
 		
+		tmpl, err := template.ParseFiles(fp)
 	    
+	    if err != nil {
+		    http.Error(w, err.Error(), http.StatusInternalServerError)
+		    return
+	    }
+		
+		if err := tmpl.Execute(w, "no data needed"); err != nil {
+		    http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+    } else {
+	    //	start := time.Now()
 		//r.ParseMultipartForm(32 << 20) // limit your max input length!
 		
 		r.Body = http.MaxBytesReader(w, r.Body, 32<<20+512)
@@ -45,7 +61,7 @@ func upload (w http.ResponseWriter, r *http.Request) {
 		file, header, err := r.FormFile("file")
 		
 		submitTime := r.FormValue("submitTime")		
-	    
+		
 		if err != nil {
 		    panic(err)
 		}
@@ -67,22 +83,54 @@ func upload (w http.ResponseWriter, r *http.Request) {
 		
 		stInt, err := strconv.ParseInt(submitTime,10,64)
 		
-		subTimeObj := time.Unix(0, stInt * int64(1000000))
-
-		elapsed := time.Since(subTimeObj)
-	   
 	   	fmt.Println("SubmitTime %s", time.Unix(0, stInt * int64(1000000)).Format("2006-01-02 03:04:05 PM"))
 
-		fmt.Println("Binomial took %s", elapsed.Milliseconds())
+		uploadResult := UploadResult{submitTime, name[0], strconv.FormatInt(size, 10)}
 		
-		w.Header().Set("diffResult", submitTime)
+	    fp := path.Join("templates", "upload_success.html")
+	    
+	    tmpl, err := template.ParseFiles(fp)
+	    
+	    if err != nil {
+		    http.Error(w, err.Error(), http.StatusInternalServerError)
+		    return
+	    }
 		
-		http.ServeFile(w, r, "upload/upload.html")
+		if err := tmpl.Execute(w, uploadResult); err != nil {
+		    http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
     }
 }
 
+//Save REsult and save in temp file 
+func saveResult (w http.ResponseWriter, r *http.Request) {
+	
+	f, err := os.OpenFile("log_time.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	
+	defer f.Close()
+	
+	s:=
+	"\n---------------------------------------\n"+ 
+	"File name: " + r.FormValue("fileName") + 		
+	"\n Submit Time: " + r.FormValue("subTime") + 		
+	"\n File Size: " + r.FormValue("fileSize") + 		
+	"\n Binomial took: " + r.FormValue("binomialTook") + " ms";	
+	
+	
+	
+	if _, err := f.WriteString(s); err != nil {
+		log.Println(err)
+	}
+
+}
+
+
 func readlog (w http.ResponseWriter, r *http.Request) {
-	 content, err := ioutil.ReadFile("out.log")
+	 content, err := ioutil.ReadFile("log_time.log")
 
      if err != nil {
           log.Fatal(err)
